796590ad50449a4792ba5848539b9b5841fab0ba
Yujing <vit7david@outlook.com>
2020-05-25 20:14:25 +0300

    Make annotation for each bar

--- src/components/chart/HorizontalChart/HorizontalChart.js
+++ src/components/chart/HorizontalChart/HorizontalChart.js
@@ -9,18 +9,26 @@ function HorizontalChart({ colors }) {
         useEffect(() => {
             Plotly.newPlot(divRef.current, data, layout, layout)
         }, [divRef]);
+
+        var xdata = [20, 14, 23]
+        var ydata = [0, 1, 2]
+        var textList = [20, 14, 23]
         var data = [{
             type: 'bar',
             x: [20, 14, 23],
             y: ['Calories', 'Fat', 'Protein'],
             orientation: 'h',
+            // text: ["70%", "44%", "80%"].map(String),
+            hoverinfo: 'none',
+            // textposition: 'auto',
             marker: {
                 color: colors,
-                width: 1
             },
         }];
 
         var layout = {
+            annotations: [
+            ],
             font: {
                 size: 14,
                 color: "#7b7da0",
@@ -32,8 +40,23 @@ function HorizontalChart({ colors }) {
             height: 300,
             grid: { rows: 1, columns: 1 },
             margin: { l: 60, r: 0, t: 25, b: 25 },
-            showLegend: true,
+            // showLegend: true,
         }
+
+        xdata.map((item, index) => {
+            var result = {
+                x: xdata[index] - 1,
+                y: ydata[index],
+                text: textList[index],
+                font: {
+                    family: 'Arial',
+                    size: 14,
+                    color: 'rgba(245,246,249,1)'
+                },
+                showarrow: false
+            };
+            layout.annotations.push(result);
+        })
 
         return graphDiv
     }
